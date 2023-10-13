c9ff2c32b05b4f13a1d48bc791ecfd652fbee267
miracle-commerce <foxshopify0127@gmail.com>
2023-10-13 05:53:20 -0400

    Create new product template

--- assets/custom.js
+++ assets/custom.js
@@ -26,6 +26,55 @@
 // Script for weight pack selector
 const ProductWeightPackSelector = document.querySelector("#product__weightPackSelector");
 
-ProductWeightPackSelector.addEventListener('click', (e)=>{
-    ProductWeightPackSelector.classList.toggle("active");
+if(ProductWeightPackSelector){
+    ProductWeightPackSelector.addEventListener('click', (e)=>{
+        ProductWeightPackSelector.classList.toggle("active");
+    })
+}
+
+// Script for ProductTabs
+const ProductTabs = document.querySelector("[data-product-tabs]");
+const ProductTabTitles = ProductTabs.querySelectorAll("[data-product-tab-title]");
+ProductTabTitles.forEach((tabTitle)=>{
+    tabTitle.addEventListener('click', (e)=>{
+        if(!tabTitle.classList.contains('active')){
+            let oldActiveTabTitle = ProductTabs.querySelector(".active[data-product-tab-title]");
+            // Deactive old actived TabTitle
+            oldActiveTabTitle.classList.remove("active");
+            // Deactive old active tabcontent
+            let oldTarget = oldActiveTabTitle.dataset.tabTarget;
+            let oldTargetContentEl = ProductTabs.querySelector(`[data-tab-for='${oldTarget}']`);
+            if(oldTargetContentEl && oldTargetContentEl.classList.contains('active')){
+                oldTargetContentEl.classList.remove('active');
+            }
+            //active new tabtitle
+            tabTitle.classList.add("active");
+            // Active new TabContent
+            let newTarget = tabTitle.dataset.tabTarget;
+            let newTargetEl = ProductTabs.querySelector(`[data-tab-for='${newTarget}']`);
+            if(newTargetEl){
+                newTargetEl.classList.add("active");
+            }
+        }
+    })
+})
+
+const Faqs = document.querySelectorAll("[data-faqs]");
+Faqs.forEach((faq)=>{
+    let faqItems = faq.querySelectorAll("[data-faq-item]");
+    faqItems.forEach((faqItem)=>{
+        let faqItemTitle = faqItem.querySelector("[data-faq-title]");
+        faqItemTitle.addEventListener('click', (e)=>{
+            faqItem.classList.toggle('active');
+        })
+    })
+})
+
+// Styles for ProductTabColumns 
+const ProductTabColumnItems = document.querySelectorAll(".ProductTabColumnItem");
+ProductTabColumnItems.forEach((ProductTabColumnItem)=>{
+    let columnTitle = ProductTabColumnItem.querySelector(".ProductTabColumnItem__Title");
+    columnTitle.addEventListener('click', (e)=>{
+        ProductTabColumnItem.classList.toggle("active");
+    })
 })
